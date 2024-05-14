e8fda43063e4a7850409fdd1ad4a6319062a1616
mkamaroski <matheushavery@hotmail.com>
2024-05-14 15:07:29 -0300

    configuration commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        greenmg: "#01CF02",
+      },
+      fontFamily: {
+        telegraf: ["Telegraf", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
